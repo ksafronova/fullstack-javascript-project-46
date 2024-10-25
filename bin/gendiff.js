9357c04dd1e8cf3bc6f30c60b1a2514931d31357
Kseniia Safronova <safronovaksusha96@gmail.com>
2024-10-25 23:23:22 +0400

    add -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 import { program } from "commander";
 
+
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1');
+
+  program
+  .helpOption('-h, --help', 'output usage information')
+
+program
+  .option('-f, --format [type]', 'output format')
+  .argument('<type>')
+
 
 program.parse();
